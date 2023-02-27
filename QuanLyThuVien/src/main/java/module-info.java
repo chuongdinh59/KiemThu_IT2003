e80003225733fcf241b1d6a3d31799dad95c7a47
Dinh Chuong <chuongdinh2202@gmail.com>
2023-02-27 20:57:36 +0700

    fix structure

---
+++ QuanLyThuVien/src/main/java/module-info.java
@@ -0,0 +1,9 @@
+module com.bros.quanlythuvien {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.base;
+    requires javafx.graphics;
+
+    opens com.bros.quanlythuvien to javafx.fxml;
+    exports com.bros.quanlythuvien;
+}
--- TheDocGia/src/main/java/module-info.java
+++
@@ -1,7 +0,0 @@
-module com.mycompany.thedocgia {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-    opens com.mycompany.thedocgia to javafx.fxml;
-    exports com.mycompany.thedocgia;
-}
