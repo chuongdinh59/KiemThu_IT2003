44218274be01879afdb74e5c25fbc5ab9a8510af
PhuNguyen8902 <89370742+PhuNguyen8902@users.noreply.github.com>
2023-02-27 22:28:06 +0700

    doing admin

---
+++ QuanLyThuVien/src/main/java/com/bros/quanlythuvien/DocGiaController.java
@@ -0,0 +1,38 @@
+package com.bros.quanlythuvien;
+
+import java.io.IOException;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
+public class DocGiaController {
+
+    @FXML
+    private void switchToQuanTri() throws IOException {
+        App.setRoot("QuanTriUI");
+    }
+    @FXML
+    private Label lbMaDG;
+    @FXML
+    private Label lbDoiTuong;
+    @FXML
+    private Label lbNgaySinh;
+    @FXML
+    private Label lbGioiTinh;
+    @FXML
+    private Label lbBoPhan;
+    @FXML
+    private Label lbHanThe;
+    @FXML
+    private Label lbHoTen;
+    @FXML
+    private TextField txtFDiaChi;
+    @FXML
+    private TextField txtFEmail;
+    @FXML
+    private TextField txtFDienThoai;
+    @FXML
+    private Button DocGiaButton;
+
+}
